09ebef3d0eb5cc28f8dc09b60c51fe5c13727574
sebastian <sebastian.ramed@gmail.com>
2022-02-25 02:41:42 -0600

    forecast displayed

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Alert, Image, StyleSheet, Text, View, useWindowDimensions } from 'react-native';
 import {FontAwesome5} from '@expo/vector-icons';
 import { ScrollView, TextInput, TouchableOpacity } from 'react-native-gesture-handler';
@@ -6,17 +6,43 @@ import axios from 'axios';
 import { useFonts, DancingScript_600SemiBold } from '@expo-google-fonts/dancing-script';
 import AppLoading from 'expo-app-loading';
 import {REACT_APP_WEATHER_API_KEY} from '@env';
+const kelvin = 272.15;
 
 export default function Home(props){
     const [loading, setLoading] = useState(false);
     const [cityObj, setCityObj] = useState(null);
     const [daysList, setDaysList] = useState([]);
+    const [currentDay, setCurrentDay] = useState(null);
     const [city, setCity] = useState("");
     let [fontsLoaded] = useFonts({
         DancingScript_600SemiBold,
       });
       const windowHeight = useWindowDimensions().height;
       const windowWidth = useWindowDimensions().width;
+      const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      const weatherIcons = {
+          'Thunderstorm': <FontAwesome5 style={{paddingLeft: 10}} name="thunderstorm" size={30} color='#c4b402'/>,
+          'Drizzle' : <FontAwesome5 style={{paddingLeft: 10}} name="cloud-drizzle" size={30} color='#05a0ab'/>,
+          'Rain' : <FontAwesome5 style={{paddingLeft: 10}} name="cloud-rain" size={30} color='#05a0ab'/>,
+          'Snow' : <FontAwesome5 style={{paddingLeft: 10}} name="snowflake" size={30} color='#65e2eb'/>,
+          'Mist' : <FontAwesome5 style={{paddingLeft: 10}} name="fog" size={30} color='#8b8f8c'/>,
+          'Clear' : <FontAwesome5 style={{paddingLeft: 10}} name="sun" size={30} color='#f29b29'/>,
+          'Clouds' : <FontAwesome5 style={{paddingLeft: 10}} name="cloud" size={30} color='#8b8f8c'/>,
+          'Smoke' : <FontAwesome5 style={{paddingLeft: 10}} name="smoke" size={30} color='#8b8f8c'/>,
+          'Haze' : <FontAwesome5 style={{paddingLeft: 10}} name="sun-haze" size={30} color='#f29b29'/>,
+          'Dust' : <FontAwesome5 style={{paddingLeft: 10}} name="sun-dust" size={30} color='#cf8f3c'/>,
+          'Fog' : <FontAwesome5 style={{paddingLeft: 10}} name="fog" size={30} color='#8b8f8c'/>,
+          'Sand' : <FontAwesome5 style={{paddingLeft: 10}} name="cactus" size={30} color='#cf8f3c'/>,
+          'Ash' : <FontAwesome5 style={{paddingLeft: 10}} name="volcano" size={30} color='#8b8f8c'/>,
+          'Squall' : <FontAwesome5 style={{paddingLeft: 10}} name="hurricane" size={30} color='#002674'/>,
+          'Tornado' : <FontAwesome5 style={{paddingLeft: 10}} name="tornado" size={30} color='#8b8f8c'/>,
+      }
+
+      useEffect(() => {
+        let date = new Date();
+        setCurrentDay(date.getDay());
+        console.log(date.getDay());
+      }, []);
 
       const searchCity = async () => {
           if(city != ""){
@@ -50,7 +76,6 @@ export default function Home(props){
             params: { lat : cityData.lat, lon: cityData.long, appid: REACT_APP_WEATHER_API_KEY} 
         })
         .then(json => {
-            console.log(json.data);
             if(json.data){
 
                setCityObj(cityData);
@@ -69,6 +94,37 @@ export default function Home(props){
             setLoading(false);
         }
         );
+    }
+
+    const DaysMap = () => {
+        let nextDay = currentDay + 1;
+        return <ScrollView>
+            {daysList.map((day, dayKey) => {
+            console.log(day);
+            if((nextDay + 1) > 6){
+                nextDay = 0;
+            } else {
+                nextDay++;
+            }
+            return <View key={dayKey} style={styles.daysItem}>
+                <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                    <Text style={{fontSize : 18, fontWeight: 'bold'}}>{nextDay != 0 ? weekDays[nextDay - 1] : weekDays[6]}</Text>
+                    {weatherIcons[day.weather[0].main]}
+                </View>
+                <View style={{flexDirection: 'row', justifyContent: 'space-around', marginTop: 5}}>
+                    <View style={{alignItems: 'center'}}>
+                        <Text style={{fontSize: 16, fontWeight: 'bold'}}>{`${Math.round(day.temp.max - kelvin)}°`}</Text>
+                        <Text style={{fontSize: 14, fontStyle: 'italic'}}>Max.</Text>
+                    </View>
+                    <View style={{alignItems: 'center'}}>
+                        <Text style={{fontSize: 16, fontWeight: 'bold'}}>{`${Math.round(day.temp.min - kelvin)}°`}</Text>
+                        <Text style={{fontSize: 14, fontStyle: 'italic'}}>Min.</Text>
+                    </View>
+                </View>
+            </View>
+        }
+        )}
+        </ScrollView>;
     }
 
       if (!fontsLoaded) {
@@ -110,9 +166,19 @@ export default function Home(props){
                                 borderColor: '#002674',
                                 borderWidth: 2,
                                 borderRadius: 10,
-                                flexDirection: 'column'}
+                                flexDirection: 'column',
+                                justifyContent: 'space-around',
+                                alignItems: 'center'
+                            }
                             }>
-                                <Text>{cityObj.city_name}</Text>
+                                <Text style={{fontFamily: 'Roboto', fontWeight: 'bold', fontSize: 30, 
+                                    color: '#002674'}}>
+                                    {cityObj.city_name}
+                                </Text>
+                                <View style={{flex: 1, marginVertical: 15, width: '90%'}}>
+                                    <DaysMap/>
+                                </View>
+                                
                             </View>
                         </View>
                         : 
@@ -169,19 +235,6 @@ let styles = StyleSheet.create({
         width: '90%',
         height: '35%'
     },
-    boton:{
-        backgroundColor: '#adc867',
-        padding: 5,
-        borderRadius: 5,
-        color: '#FFF',
-        fontSize: 20
-    },
-    label: {
-        alignSelf: 'flex-start',
-        fontWeight: 'bold',
-        marginLeft: 10,
-        marginBottom: -25
-    },
     reservamosLogo: {
         width: 150,
         height: 60,
@@ -193,11 +246,14 @@ let styles = StyleSheet.create({
         height: 30,
         resizeMode: 'contain'
     },
-    cityItem: {
+    daysItem: {
         height: '90%',
-        borderColor: '#002674',
-        borderWidth: 2,
+        borderColor: '#8b8f8c',
+        borderWidth: 1,
         borderRadius: 10,
-        flexDirection: 'column'
+        flexDirection: 'column',
+        flex: 1,
+        marginVertical: 10,
+        padding: 10
     }
   });
